ee39f14c6b53ca5ff1f3b3727958867f97b84717
jpcapobiango <joao.capobiango213@gmail.com>
2024-07-09 19:39:15 -0300

    adding basic security configuration

---
+++ src/main/java/com/joaocapobiango/coursesbackend/security/SecurityConfiguration.java
@@ -0,0 +1,17 @@
+package com.joaocapobiango.coursesbackend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(csrf -> csrf.disable());
+        return httpSecurity.build();
+    }
+
+}
